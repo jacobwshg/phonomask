2f4a364460ba04d6cc6e860869ed081c810e0540
jacobwshg <wsg2002@hotmail.com>
2025-09-10 13:12:28 -0500

    added dict for common features to aliases

---
+++ alias.py
@@ -0,0 +1,25 @@
+feature_aliases = {
+    'consonantal': ['cons'],
+    'syllabic': ['syl', 'syll'],
+    'sonorant': ['son'],
+    'nasal':['nas'],
+    'labial': ['lab'],
+    'coronal': ['coronal'],
+    'anterior': ['ant'],
+    'distributed': ['dist', 'distr'],
+    'strident': ['stri', 'strident'],
+    'high': ['hi'],
+    'low': ['lo'],
+    'back': ['bk', 'bac'],
+    'lateral': ['lat'],
+    'continuant': ['cont'],
+    'voice': ['v', 'vce', 'voi'],
+    'DR': ['delrel', 'del_rel, del rel'],
+    'CG': ['c.g.'],
+    'SG': ['s.g.'],
+}
+
+__all__ = [\
+    'feature_aliases',\
+]
+
