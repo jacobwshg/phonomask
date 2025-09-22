3332bfd5c3a912a1b4947c5a8c5dc1d0e69d2516
jacobwshg <wsg2002@hotmail.com>
2025-09-22 13:01:41 -0500

    corrected apply indexing

--- cpp/apply.cpp
+++ cpp/apply.cpp
@@ -9,23 +9,25 @@ void apply(const Phmask::Rule &rule, std::vector<Phmask::feat_mtx_t> &word_fms)
     std::size_t wordlen {word_fms.size()},
                 xlen {rule.X.size()},
                 ylen {rule.Y.size()};
-    for (std::size_t i {xlen + 1}; i < wordlen - ylen; ++i)
+    for (std::size_t i {xlen}; i < wordlen - ylen; ++i)
     {
         Phmask::feat_mtx_t cur_seg_fm {word_fms[i]};
+        std::size_t xbegin {i - xlen};
+        std::size_t ybegin {i + 1};
         if (!rule.A.test(cur_seg_fm))
         {
             goto done;
         }
         for (std::size_t x_i {0}; x_i < xlen; ++x_i)
         {
-            if (!rule.X[x_i].test(word_fms[i - 1 - x_i]))
+            if (!rule.X[x_i].test(word_fms[xbegin + x_i]))
             {
                 goto done;
             }
         }
         for (std::size_t y_i {0}; y_i < ylen; ++y_i)
         {
-            if (!rule.Y[y_i].test(word_fms[i - 1 - y_i]))
+            if (!rule.Y[y_i].test(word_fms[ybegin + y_i]))
             {
                 goto done;
             }
@@ -36,5 +38,4 @@ void apply(const Phmask::Rule &rule, std::vector<Phmask::feat_mtx_t> &word_fms)
             continue;
     }
 }
-
 
