4ef1a5716fac4c03850e7669cadbc291392eb01e
jacobwshg <wsg2002@hotmail.com>
2025-09-30 23:50:33 -0500

    condensed svutils and removed unused files

--- cpp/svutils.h
+++ cpp/svutils.h
@@ -8,16 +8,36 @@
 struct SvStrHash
 {
     using is_transparent = void;
-    std::size_t operator()(const std::string_view) const;
-    std::size_t operator()(const std::string &) const;
+
+    std::size_t operator()(const std::string_view sv) const
+    {
+        return std::hash<std::string_view>{}(sv);
+    }
+
+    std::size_t operator()(const std::string &s) const
+    {
+        return std::hash<std::string>{}(s);
+    }
 };
 
 struct SvStrEq
 {
     using is_transparent = void;
-    bool operator()(const std::string_view, const std::string_view) const;
-    bool operator()(const std::string_view, const std::string &) const;
-    bool operator()(const std::string &, const std::string &) const;
+
+    bool operator()(const std::string_view sv1, const std::string_view sv2) const
+    {
+        return sv1 == sv2;
+    }
+
+    bool operator()(const std::string_view sv, const std::string &s) const
+    {
+        return sv == s;
+    }
+
+    bool operator()(const std::string &s1, const std::string &s2) const
+    {
+        return s1 == s2;
+    }
 };
 
 #endif
