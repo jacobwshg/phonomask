4d874949e9a2177abe1f438d75b302de92d3bcd1
jacobwshg <wsg438@gmail.com>
2026-03-14 18:29:19 -0500

    added build script

---
+++ cpp/build-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ./*.out
+
+clang++ -Wall -Wextra -std=c++20 -o phmask.out \
+  ./utils.cpp \
+  ./seg_fm_maps.cpp ./feat_idx_maps.cpp ./feature_profile.cpp \
+  ./rule.cpp ./word.cpp \
+  ./test.cpp -licuuc -licudata 
+
